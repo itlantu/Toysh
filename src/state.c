af176b02f94e481fd8056dfba901f1c8b3359dc9
lantu <it_lantu@outlook.com>
2026-04-21 23:29:36 +0800

    feat: 添加状态名称获取函数

---
+++ src/state.c
@@ -0,0 +1,11 @@
+#include "toysh/state.h"
+
+#define X(name) #name,
+static const char* ToyshStateStrings[TOYSH_ENUM_LENGTH] = {TOYSH_STATE_ENUM()};
+#undef X
+
+const char* get_state_name(const ToyshState state){
+    if (state >= TOYSH_ENUM_LENGTH)
+        return "UNKNOWN";
+    return ToyshStateStrings[state];
+}
