04be70184c8a5806ecb908dc6b44e625e6f9ad3a
lantu <it_lantu@outlook.com>
2026-04-24 23:35:17 +0800

    refactor: 重构command模块, 实现了MSVC编译器下编译通过

--- include/toysh/command.h
+++ include/toysh/command.h
@@ -3,52 +3,55 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif  //  __cplusplus
+#endif
 
-#include <stdint.h>
+#include <stddef.h>
 
 typedef int (*ToyshFunc)(int argc, char** argv);
 
-typedef struct ToyshCommand {
+struct ToyshCommand {
     const char* name;
-    const char* help;
+    const char* desc;
     ToyshFunc func;
-}ToyshCommand;
+};
 
-#if defined(_MSC_VER) && !defined(__clang__)
+extern const struct ToyshCommand __start_toysh;
+extern const struct ToyshCommand __stop_toysh;
 
-#pragma section("toysh_section$a", read)
-#pragma section("toysh_section$b", read)
-#pragma section("toysh_section$c", read)
-__declspec(allocate("toysh_section$a"))
-static const ToyshCommand* __start_toysh_section = NULL;
-__declspec(allocate("toysh_section$c"))
-static const ToyshCommand* __stop_toysh_section = NULL;
-#define TOYSH_EXPORT_COMMAND(name, help, func) \
-    static const ToyshCommand _toysh_cmd ## __COUNTER__ ## __LINE__ = {name, help, func};\
-    __declspec(allocate("toysh_section$b")) \
-        static const ToyshCommand* _toysh_ptr ## __COUNTER__ ## __LINE__ = &_toysh_cmd ##  __COUNTER__ ## __LINE__
-#define TOYSH_SECTION_BEGIN()   ((const ToyshCommand**)(&__start_toysh_section + 1))
-#define TOYSH_SECTION_END()     ((const ToyshCommand**)(&__stop_toysh_section))
-
+#ifdef _MSC_VER
+#pragma section(".rdata$toysh$a", read)
+#pragma section(".rdata$toysh$m", read)
+#pragma section(".rdata$toysh$z", read)
+#define TOYSH_COMMAND_INIT()    \
+    __declspec(allocate(".rdata$toysh$a"))  \
+    const struct ToyshCommand __start_toysh = {"__start", "begin of section", NULL};    \
+    __declspec(allocate(".rdata$toysh$z"))  \
+    const struct ToyshCommand __stop_toysh = {"__stop", "end of section", NULL}
 #else
-
-#define TOYSH_EXPORT_COMMAND(name, help, func) \
-    static ToyshCommand _toysh_cmd_ ## __COUNTER__ ## __LINE__ \
-    __attribute__((section("toysh_section"))) = {name, help, func}
-extern const ToyshCommand __start_toysh_section;
-extern const ToyshCommand __stop_toysh_section;
-#define TOYSH_SECTION_BEGIN()   ((const ToyshCommand**)&__start_toysh_section)
-#define TOYSH_SECTION_END()     ((const ToyshCommand**)&__stop_toysh_section)
-
+#define TOYSH_COMMAND_INIT()
 #endif
 
-unsigned int toysh_get_command_count();
-const ToyshCommand* toysh_find_command(const char* name);
+#if defined(_MSC_VER)
+#define TOYSH_CMD_EXPORT(name, func, desc)  \
+    const char __toysh_cmd_name_##name[] = #name;   \
+    const char __toysh_cmd_desc_##name[] = desc;    \
+    __declspec(allocate(".rdata$toysh$m"))  \
+    const struct ToyshCommand __toysh_cmd_object##name = {#name, desc, func};
+#else
+// todo
+#endif
+
+#define TOYSH_COMMAND_SECTION_BEGIN()  ((const struct ToyshCommand*)&__start_toysh)
+#define TOYSH_COMMAND_SECTION_END()    ((const struct ToyshCommand*)&__stop_toysh)
+
+const struct ToyshCommand* toysh_command_section_begin();
+const struct ToyshCommand* toysh_command_section_end();
+
+unsigned int toysh_command_get_count();
+const struct ToyshCommand* toysh_command_find(const char* name);
 
 #ifdef __cplusplus
 }
-#endif  //  __cplusplus
+#endif
 
-
-#endif  //  TOYSH_COMMAND_H
+#endif  /* TOYSH_COMMAND_H */
